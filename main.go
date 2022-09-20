f45daed8958aa471682d8634c4308eecfd6014e7
Robin Hood <yoga3911@gmail.com>
2022-09-20 14:48:18 +0700

    update

--- main.go
+++ main.go
@@ -130,12 +130,11 @@ func main() {
 	e := echo.New()
 	// routing with query parameter
 	e.GET("/", root)
-	api := e.Group("/api/v1")
-	api.GET("/users", GetUsersController)
-	api.GET("/users/:id", GetUserController)
-	api.POST("/users", CreateUserController)
-	api.DELETE("/users/:id", DeleteUserController)
-	api.PUT("/users/:id", UpdateUserController)
+	e.GET("/users", GetUsersController)
+	e.GET("/users/:id", GetUserController)
+	e.POST("/users", CreateUserController)
+	e.DELETE("/users/:id", DeleteUserController)
+	e.PUT("/users/:id", UpdateUserController)
 
 	// start the server, and log if it fails
 	e.Logger.Fatal(e.Start(":8000"))
